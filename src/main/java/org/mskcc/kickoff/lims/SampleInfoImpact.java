ec84d0c57a939438ea61a54097e32fa6bb8184f1
Ewa Reza <reza@alba.cbio.private>
2018-10-09 17:15:17 -0400

    [IGOWEB-1421] Optimize retrieving poolednormals

--- src/main/java/org/mskcc/kickoff/lims/SampleInfoImpact.java
+++ src/main/java/org/mskcc/kickoff/lims/SampleInfoImpact.java
@@ -51,6 +51,7 @@ public class SampleInfoImpact extends SampleInfo {
     private String CAPTURE_CONCENTRATION; // = "#EMPTY";
     private String CAPTURE_NAME; // = "#EMPTY";
     private String SPIKE_IN_GENES; // = "na";
+    private static List<DataRecord> potentialPooledNormalsQcs;
 
     /**
      * This is the method that CreateManifestSheet calls.<br>First it looks at parent class's method, then uses new
@@ -991,13 +992,8 @@ public class SampleInfoImpact extends SampleInfo {
 
     private void populatePoolNormals(User apiUser, DataRecordManager dataRecordManager, KickoffRequest kickoffRequest) {
         try {
-            String query = String.format("%s LIKE '%s'", VeloxConstants.OTHER_SAMPLE_ID,
-                    "%POOLEDNORMAL%", VeloxConstants.SEQUENCER_RUN_FOLDER);
-
-            DEV_LOGGER.info(String.format("Query used to look for pooled normals: %s", query));
-
-            List<DataRecord> potentialPooledNormalsQcs = dataRecordManager.queryDataRecords(VeloxConstants
-                    .SEQ_ANALYSIS_SAMPLE_QC, query, apiUser);
+            if(potentialPooledNormalsQcs == null)
+                potentialPooledNormalsQcs = getPotentialPooledNormalQCs(apiUser, dataRecordManager);
 
             for (DataRecord potentialPooledNormalQc : potentialPooledNormalsQcs) {
                 if (!isSampleRun(potentialPooledNormalQc, apiUser, kickoffRequest))
@@ -1057,6 +1053,16 @@ public class SampleInfoImpact extends SampleInfo {
             DEV_LOGGER.error("Exception thrown wile retrieving information about pooled normals", e);
         }
     }
+
+    private List<DataRecord> getPotentialPooledNormalQCs(User apiUser, DataRecordManager dataRecordManager) throws Exception {
+        String query = String.format("%s LIKE '%s'", VeloxConstants.OTHER_SAMPLE_ID,
+                "%POOLEDNORMAL%", VeloxConstants.SEQUENCER_RUN_FOLDER);
+
+        DEV_LOGGER.info(String.format("Query used to look for pooled normals: %s", query));
+        return dataRecordManager.queryDataRecords(VeloxConstants.SEQ_ANALYSIS_SAMPLE_QC,
+                query, apiUser);
+    }
+
 
     private boolean isSampleRun(DataRecord potentialPooledNormalQc, User apiUser, KickoffRequest kickoffRequest)
             throws NotFound, RemoteException {
